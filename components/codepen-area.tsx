01863a278cb4960454033583d911c0b62c7ccac3
sifatul <sifatul@teraark.com>
2022-07-26 13:41:37 +0900

    fix : type error fixed

--- components/codepen-area.tsx
+++ components/codepen-area.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import { useCallback, useEffect, useState } from "react";
 import { SearchByType } from '../types/common.types';
 import { GetData } from "../Utils/fetchData";
+interface codepenItemType {
+  pubDate: string
+  link: string
+  title: string
+}
 
 const codepenInfoFetchUrl = 'https://api.rss2json.com/v1/api.json?rss_url=https://codepen.io/userName/popular/feed/'
 const CodePenArea = (props: any) => {
   const { originalSearchVal, searchBy, pathname } = props
-
+  const [popularPen, setPopularPen] = useState<codepenItemType[]>([])
   let codePenUserName = originalSearchVal
   // considering the input to be a name
   if (searchBy === SearchByType.URL) {
@@ -15,13 +20,15 @@ const CodePenArea = (props: any) => {
   }
   if (!codePenUserName) return <></>
   const codepenInfoFetchApi = codepenInfoFetchUrl.replace('userName', codePenUserName)
-  const [popularPen, setPopularPen] = useState([])
+
 
   const getCodepenData = useCallback(async () => {
     const data: any = await GetData(codepenInfoFetchApi)
     const { items = [] } = data
-    const sortedData = items.sort((a, b) => {
-      return b.pubDate - a.pubDate
+    const sortedData: codepenItemType[] = items.sort((a: { pubDate: string; }, b: { pubDate: string; }) => {
+      const timeA = new Date(a.pubDate).getTime()
+      const timeB = new Date(b.pubDate).getTime()
+      return timeB - timeA
     })
     setPopularPen(sortedData.slice(0, 2))
 
@@ -37,6 +44,7 @@ const CodePenArea = (props: any) => {
   return <div style={{ display: 'flex' }}>
     {
       popularPen.map((item, idx) => {
+        if (!item) return <></>
 
         const codepenLink = item?.link
         if (!codepenLink) return <></>
@@ -49,8 +57,9 @@ const CodePenArea = (props: any) => {
         const userName = pathnameArr[0]
         const projectName = pathnameArr.pop()
         const previewUrl = `https://codepen.io/${userName}/embed/preview/${projectName}`
-        return <iframe height="300" scrolling="no" title="Stacking Cards (Motion One Version)" src={previewUrl}
-          frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+        return <iframe height="300" key={'ifram-' + idx}
+          scrolling="no" title={item?.title} src={previewUrl}
+          frameBorder="no" loading="lazy" allowTransparency={true} allowFullScreen={true}>
           See the Pen <a href={codepenLink}>
             Stacking Cards (Motion One Version)</a> by Bramus (<a href={codepenLink}>@bramus</a>)
 on <a href={codepenLink}>CodePen</a>.
