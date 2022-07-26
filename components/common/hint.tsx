59b9df2a448bb9f7c81cc8d52cb45eb216f82ac4
sifatul <sifatul@teraark.com>
2022-07-26 13:52:31 +0900

    fix: alt added to image

--- components/common/hint.tsx
+++ components/common/hint.tsx
@@ -7,9 +7,15 @@ const Hint = () => {
 
     <p> Now you can get a summary of developers profile by inserting </p>
     <div>Profile <strong>user name</strong> or <strong>profile link</strong> from:</div>
-    <Image src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" width="100px" height="30px" />
-    <Image src="https://img.shields.io/badge/-Hackerrank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white" width="100px" height="30px" />
-    <Image src="https://img.shields.io/badge/Codepen-000000?style=for-the-badge&logo=codepen&logoColor=white" width="100px" height="30px" />
+    <Image
+      alt="github icon"
+      src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white" width="100px" height="30px" />
+    <Image
+      alt="hackerrank icon"
+      src="https://img.shields.io/badge/-Hackerrank-2EC866?style=for-the-badge&logo=HackerRank&logoColor=white" width="100px" height="30px" />
+    <Image
+      alt="codepen icon"
+      src="https://img.shields.io/badge/Codepen-000000?style=for-the-badge&logo=codepen&logoColor=white" width="100px" height="30px" />
 
 
   </div >
