5e8d432998969920f7564fb8fa384f9f12e7661f
sifatul <sifatul@teraark.com>
2022-08-08 14:37:05 +0900

    mongodb connection addded

---
+++ pages/api/database-fetch-example.ts
@@ -0,0 +1,14 @@
+import { connectToDatabase } from "../../Utils/mongodb";
+
+export default async (req, res) => {
+  const { db } = await connectToDatabase();
+
+  const movies = await db
+    .collection("users")
+    .find({})
+    .sort({ metacritic: -1 })
+    .limit(20)
+    .toArray();
+
+  res.json(movies);
+};
