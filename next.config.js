e34f49529f9d23f6d7e6edaa47221e294e3c992f
fazleteraark <fazle@teraark.com>
2022-08-24 01:08:28 +0600

    add swagger doc in users api fix npm build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['img.shields.io'],
   },
