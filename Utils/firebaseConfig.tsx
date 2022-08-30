0316eef3f0e276842b695f5e35480c2d65de947f
sifatul <sifatul@teraark.com>
2022-08-30 09:37:20 +0900

    firebase config

---
+++ Utils/firebaseConfig.tsx
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+}
+
+
+
+let firebaseAnalytics; let firebaseApp;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+if (app.name && typeof window !== 'undefined') {
+  firebaseAnalytics = getAnalytics(firebaseApp);
+}
+
+
+export { firebaseAnalytics }
