907650af2830e0ab5654f1f4d6c679e1d89b4256
fazlerabby07 <fazlerabby07@gmail.com>
2022-09-14 12:22:59 +0600

    add index in userId in review schema

--- pages/api/reviews/model/ReviewSchema.ts
+++ pages/api/reviews/model/ReviewSchema.ts
@@ -1,7 +1,7 @@
 import { model, models, Schema, Types } from 'mongoose';
 
 interface IReview {
-    revieweeId: Types.ObjectId;
+    userId: Types.ObjectId;
     reviewerId: Types.ObjectId;
     pros: string;
     cons: string;
@@ -9,7 +9,7 @@ interface IReview {
 
 const ReviewSchema = new Schema<IReview>(
     {
-        revieweeId: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
+        userId: { type: Schema.Types.ObjectId, required: true, ref: 'users', index: true },
         reviewerId: { type: Schema.Types.ObjectId, required: true, ref: 'users' },
         pros: { type: String, required: true },
         cons: { type: String, required: true },
