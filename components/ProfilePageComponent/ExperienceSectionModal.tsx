6d3d4a6a618138bbe8bdfa89ba1ded085ae8169a
Juaid Rakin <juaidrakin1@gmail.com>
2022-09-15 12:32:32 +0600

    wip:experience section form

---
+++ components/ProfilePageComponent/ExperienceSectionModal.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Modal from 'react-modal';
+import ExperienceForm from './form/ExperienceForm';
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        width: '60vw',
+        height: '60vh',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+interface ModalProps {
+    openModal: () => void;
+    closeModal: () => void;
+    modalIsOpen: boolean;
+    afterOpenModal?: () => void;
+}
+
+const ExperienceSectionModal: React.FC<ModalProps> = ({
+    openModal,
+    closeModal,
+    modalIsOpen,
+    afterOpenModal,
+}) => {
+    return (
+        <div>
+            <Modal
+                className=''
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={customStyles}
+                shouldCloseOnOverlayClick={true}
+                contentLabel='Category'
+                ariaHideApp={false}
+            >
+                <div>
+                    <ExperienceForm />
+                </div>
+            </Modal>
+        </div>
+    );
+};
+
+export default ExperienceSectionModal;
