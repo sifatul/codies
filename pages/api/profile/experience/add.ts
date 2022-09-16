fdcd57e996d5c88e5bb374b4775ef8071a644f79
Juaid Rakin <juaidrakin1@gmail.com>
2022-09-16 10:20:36 +0600

    experience api done

--- pages/api/profile/experience/add.ts
+++ pages/api/profile/experience/add.ts
@@ -1,5 +1,8 @@
+import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { connectToDatabase } from '../../../../Utils/mongodb';
+import User from '../../users/models/UserSchema';
+import Experience from '../models/ExperienceSchema';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
@@ -9,12 +12,37 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             return res.status(400).json({ status: 'error', message: 'body param missing' });
         }
 
-        const { companyName, position, startDate, endDate, summary, techStack } = JSON.parse(
-            req.body
-        );
+        const { userId, companyName, position, startDate, endDate, summary, techStack } =
+            JSON.parse(JSON.stringify(req.body));
 
-        if (!companyName || !position || !startDate) {
+        if (!userId || !companyName || !position || !startDate) {
             return res.status(400).json({ status: 'error', message: 'required param missing' });
         }
-    } catch (error) {}
+
+        const query = { _id: new ObjectId(userId) };
+
+        // tslint:disable-next-line: await-promise
+        const userData = await User.findOne(query);
+
+        if (!userData) {
+            return res.status(400).json({ status: 'error', message: 'user not found' });
+        }
+
+        const newExperience = await Experience.create({
+            userId,
+            companyName,
+            position,
+            startDate,
+            endDate,
+            summary,
+            techStack,
+        });
+
+        return res
+            .status(201)
+            .json({ message: ' experience added successfully.', data: newExperience });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: 'something went wrong' });
+    }
 };
