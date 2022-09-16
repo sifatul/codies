f4a56ca05cea7756c168dcc9af60dc0e4299964f
Juaid Rakin <juaidrakin1@gmail.com>
2022-09-16 21:29:19 +0600

    fixed json status

--- pages/api/profile/experience/add.ts
+++ pages/api/profile/experience/add.ts
@@ -9,14 +9,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         await connectToDatabase();
 
         if (!req.body) {
-            return res.status(400).json({ status: 'error', message: 'body param missing' });
+            return res.status(400).json({ message: 'body param missing' });
         }
 
         const { userId, companyName, position, startDate, endDate, summary, techStack } =
             JSON.parse(JSON.stringify(req.body));
 
         if (!userId || !companyName || !position || !startDate) {
-            return res.status(400).json({ status: 'error', message: 'required param missing' });
+            return res.status(400).json({ message: 'required param missing' });
         }
 
         const query = { _id: new ObjectId(userId) };
@@ -25,7 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const userData = await User.findOne(query);
 
         if (!userData) {
-            return res.status(400).json({ status: 'error', message: 'user not found' });
+            return res.status(400).json({ message: 'user not found' });
         }
 
         const newExperience = await Experience.create({
@@ -40,9 +40,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
         return res
             .status(201)
-            .json({ message: ' experience added successfully.', data: newExperience });
+            .json({ message: 'experience added successfully.', data: newExperience });
     } catch (error) {
-        console.log(error)
         return res.status(500).json({ message: 'something went wrong' });
     }
 };
